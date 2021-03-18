9970eb5bc895e708690fe9feb1efcee3dfbb106c
Benjamin Reigner <benjamin.reigner@epitech.eu>
2021-03-18 14:42:04 +0100

    [Build] Switched to cmake
    The Makefile has been removed and replaced by a CMakeLists.txt
    A lib example is available and is named toto.

---
+++ lib/TotoLib/include/TotoClass.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2021
+** B_OOP_400_PAR_4_1_arcade_coline_seguret
+** File description:
+** TotoClass.hpp.h
+*/
+
+#pragma once
+
+
+class TotoClass {
+
+public:
+    TotoClass();
+
+    ~TotoClass();
+};
+
+
---
+++ lib/TotoLib/src/TotoClass.cpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2021
+** B_OOP_400_PAR_4_1_arcade_coline_seguret
+** File description:
+** TotoClass.cpp.cc
+*/
+
+#include <iostream>
+#include "TotoClass.hpp"
+
+TotoClass::TotoClass()
+{
+    std::cout << "TotoClass Constructor" << std::endl;
+}
+
+TotoClass::~TotoClass()
+{
+    std::cout << "TotoClass Destructor" << std::endl;
+}
