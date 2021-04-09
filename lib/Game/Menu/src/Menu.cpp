b4994651670ffd2e8588e957e3b29fb864a779e2
Martin Olivier <martin.olivier@live.fr>
2021-04-09 21:36:56 +0200

    [FIX] Dynamic Menu

--- lib/Game/Menu/src/Menu.cpp
+++ lib/Game/Menu/src/Menu.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <filesystem>
+#include <map>
 #include "Menu.hpp"
 #include "Exception.hpp"
 #include "Parsing.hpp"
@@ -9,10 +10,17 @@ extern "C" Arcade::Menu *Arcade::entry_point()
     return new Arcade::Menu;
 }
 
+std::map<std::string, std::string> m_menu_displayer_map = {
+    {"./lib/arcade_pacman.so", "assets/Menu/pacman_affiche.png"},
+    {"./lib/arcade_nibbler.so", "assets/Menu/nibbler_affiche.png"},
+    {"./lib/arcade_qix.so", "assets/Menu/other_affiche.png"},
+    {"./lib/arcade_centipede.so", "assets/Menu/other_affiche.png"},
+    {"./lib/arcade_solarfox.so", "assets/Menu/other_affiche.png"},
+};
+
 Arcade::Menu::Menu() : m_score(0), m_iterator(0)
 {
     Arcade::setTileSize(30);
-    int pos = 3;
 
     for (const auto &entry : std::filesystem::directory_iterator("./lib")) {
         for (auto gameLib : GAMES_LIBRARIES) {
@@ -23,15 +31,14 @@ Arcade::Menu::Menu() : m_score(0), m_iterator(0)
                 other.pop_back();
                 other.pop_back();
                 other.pop_back();
-                m_text_list.push_back(std::make_shared<Arcade::Text>("", WHITE, 5, pos));
-                pos += 2;
+                m_text_list.push_back(std::make_shared<Arcade::Text>("> " + other, WHITE, 10, 8));
             }
         }
     }
 
     m_menu_title = std::make_shared<Arcade::Text>("Arcade - Menu", WHITE, 16, 1);
     m_arcade = std::make_shared<Arcade::Tile>("assets/Menu/arcade.png", 0, WHITE, 3, 3);
-    m_poster = std::make_shared<Arcade::Tile>("assets/Menu/nibbler_affiche.png", 'N', WHITE, 13, 11);
+    m_poster = std::make_shared<Arcade::Tile>(m_menu_displayer_map[m_GameLibs[0]], ' ', WHITE, 13, 11);
 
     m_menu_enter = std::make_shared<Arcade::Text>("Select a game and press ENTER !", WHITE, 12, 30);
     m_menu_exit = std::make_shared<Arcade::Text>("Press ECHAP to exit Arcade", WHITE, 13, 31);
@@ -43,27 +50,24 @@ void Arcade::Menu::reset()
 
 std::vector<std::shared_ptr<Arcade::IObject>> Arcade::Menu::loop(Arcade::Input ev)
 {
-    if (ev == RIGHT) {
-        m_poster->setPath("assets/Menu/pacman_affiche.png");
-        m_poster->setSymbol('P');
+    if (ev == LEFT or ev == UP) {
         m_score--;
         if (m_score < 0)
             m_score = static_cast<int>(m_text_list.size()) - 1;
+        m_poster->setPath(m_menu_displayer_map[m_GameLibs[m_score]]);
     }
-    else if (ev == LEFT) {
-        m_poster->setSymbol('N');
-        m_poster->setPath("assets/Menu/nibbler_affiche.png");
+    else if (ev == RIGHT or ev == DOWN) {
         m_score++;
         if (m_score >= static_cast<int>(m_text_list.size()))
             m_score = 0;
+        m_poster->setPath(m_menu_displayer_map[m_GameLibs[m_score]]);
     }
     std::vector<std::shared_ptr<Arcade::IObject>> buf{};
+    buf.push_back(m_text_list[m_score]);
     buf.push_back(m_arcade);
     buf.push_back(m_poster);
     buf.push_back(m_menu_title);
     buf.push_back(m_menu_enter);
     buf.push_back(m_menu_exit);
-    for (const auto &txt : m_text_list)
-        buf.push_back(txt);
     return buf;
 }
