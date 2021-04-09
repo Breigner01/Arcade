688e2385a4c612de457e477f596633d4f61472de
Coline <coline.seguret@epitech.eu>
2021-04-09 19:48:59 +0200

    [SET PACAMAN] 30px in pacman's game

--- lib/Game/Pacman/src/Pacman.cpp
+++ lib/Game/Pacman/src/Pacman.cpp
@@ -12,7 +12,7 @@ extern "C" Arcade::Pacman *Arcade::entry_point()
 
 Arcade::Pacman::Pacman()
 {
-    Arcade::setTileSize(50);
+    Arcade::setTileSize(30);
     std::ifstream stream("assets/Pacman/map.txt");
     std::ostringstream content;
 
